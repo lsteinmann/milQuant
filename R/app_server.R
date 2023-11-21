adc1bd1c952421c8db6b19479b276b11634861c6
Lisa Steinmann <37840007+lsteinmann@users.noreply.github.com>
2023-11-21 16:30:57 +0100

    move modal creation to file

--- R/app_server.R
+++ R/app_server.R
@@ -7,27 +7,9 @@ app_server <- function(input, output, session) {
 
   #session <- getDefaultReactiveDomain()
 
-  # define the ui of the login dialog box
-  login_dialog <- modalDialog(
-    title = "Enter connection details to continue",
-    footer = actionButton("tab_connect.connect","Connect"),
-    textInput("tab_connect.host","Host (Field Desktop)", value = "127.0.0.1"),
-    textInput("tab_connect.user","Username", value = "username"),
-    passwordInput("tab_connect.pwd","Password", value = "hallo"),
-    tags$div(class = "warn-text",textOutput("tab_connect.error_msg"))
-  )
-
-
-  busy_dialog <- modalDialog(
-    title = "Loading Project, please wait...",
-    div(class = "warn-text", textOutput("load.error_msg") %>%
-          withSpinner(image = "/img/quant-spinner-smooth.gif",
-                      image.width = 120,
-                      image.height = 120,
-                      proxy.height = "100px",
-                      color.background = "#ffffff")),
-    footer = shinyjs::hidden(actionButton(inputId = "close_busy_dialog", "Close"))
-  )
+  # define the ui of the login and busy dialog boxes
+  login_dialog <- make_login_dialog()
+  busy_dialog <- make_busy_dialog()
 
   # show login dialog box when initiated
   showModal(login_dialog, session = session)
--- R/ui_modals.R
+++ R/ui_modals.R
@@ -1,2 +1,35 @@
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+make_login_dialog <- function() {
+  modalDialog(
+    title = "Enter connection details to continue",
+    footer = actionButton("tab_connect.connect","Connect"),
+    textInput("tab_connect.host","Host (Field Desktop)", value = "127.0.0.1"),
+    textInput("tab_connect.user","Username", value = "username"),
+    passwordInput("tab_connect.pwd","Password", value = "hallo"),
+    tags$div(class = "warn-text",textOutput("tab_connect.error_msg"))
+  )
+}
 
-
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+make_busy_dialog <- function() {
+  modalDialog(
+    title = "Loading Project, please wait...",
+    div(class = "warn-text", textOutput("load.error_msg") %>%
+          withSpinner(image = "/img/quant-spinner-smooth.gif",
+                      image.width = 120,
+                      image.height = 120,
+                      proxy.height = "100px",
+                      color.background = "#ffffff")),
+    footer = shinyjs::hidden(actionButton(inputId = "close_busy_dialog", "Close"))
+  )
+}
