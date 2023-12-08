baf8f767bcfbaa346de4e9b3637f8537f855dcf3
Lisa Steinmann <37840007+lsteinmann@users.noreply.github.com>
2023-12-08 17:04:03 +0100

    modify QB plot

--- R/fun_utility.R
+++ R/fun_utility.R
@@ -61,9 +61,14 @@ read_milQuant_settings <- function() {
   return(result)
 }
 
-
-# data <- result
-
+#' derive_dating_from_periods
+#'
+#' @param data
+#'
+#' @return
+#' @export
+#'
+#' @examples
 derive_dating_from_periods <- function(data) {
   data("milQuant_periods")
 
@@ -107,3 +112,108 @@ derive_dating_from_periods <- function(data) {
 
   return(data)
 }
+
+#' multiply_resources_by_period_range
+#'
+#' @param data
+#'
+#' @return
+#' @export
+#'
+#' @examples
+multiply_resources_by_period_range <- function(data, split = TRUE) {
+
+  if (split == FALSE) {
+    # a way out to switch of this function when the switch input is disable
+    return(data)
+  }
+
+  data("milQuant_periods")
+
+  needed_cols <- c("period", "period.start", "period.end")
+
+  if (!all(needed_cols %in% colnames(data))) {
+    warning(milQ_warning("Cannot derive dating from periods because columns are missing!"))
+    return(data)
+  } else if (is_milet == FALSE) {
+    warning(milQ_warning("Deriving dating from periods only works with the milet-config!"))
+    return(data)
+  }
+  new_data <- split(data, data$period)
+
+  if ("multiple" %in% names(new_data)) {
+
+    if (nrow(new_data$multiple) == 0) {
+      return(data)
+    }
+
+    multiple <- split(new_data$multiple, seq_len(nrow(new_data$multiple)))
+    names(multiple) <- new_data$multiple$identifier
+
+    multiple <- lapply(multiple, function (x) {
+      #x$period.start <- ordered(x$period.start, levels = milQuant_periods$order)
+      #x$period.end <- ordered(x$period.end, levels = milQuant_periods$order)
+      sequence <- seq(from = as.numeric(x$period.start), to = as.numeric(x$period.end))
+      new_periods <- milQuant_periods$order[sequence]
+      repl <- rep(list(x), length(new_periods))
+      repl <- do.call(bind_rows, repl)
+      repl$period <- new_periods
+      repl[, grepl("count", colnames(repl))] <- repl[, grepl("count", colnames(repl))] / length(new_periods)
+      repl
+    })
+    multiple <- do.call(bind_rows, multiple)
+
+    new_data$multiple <- multiple
+
+    new_data <- do.call(bind_rows, new_data)
+
+    return(new_data)
+  } else {
+    message("No 'multiple' available in period-column.")
+    return(data)
+  }
+}
+
+#' add_all_periods_to_all_contexts
+#'
+#' @param data
+#'
+#' @return
+#' @export
+#'
+#' @examples
+add_all_periods_to_all_contexts <- function(data) {
+  needed_cols <- c("relation.liesWithinLayer", "period")
+
+  if (!all(needed_cols %in% colnames(data))) {
+    warning(milQ_warning("Cannot derive dating from periods because columns are missing!"))
+    return(data)
+  } else if (is_milet == FALSE) {
+    warning(milQ_warning("Adding periods only works with the milet-config!"))
+    return(data)
+  }
+  sp_data <- split(data, data$relation.liesWithinLayer)
+
+  data("milQuant_periods")
+
+  sp_data <- lapply(sp_data, function (x) {
+    ind <- which(!milQuant_periods$order %in% unique(x$period))
+    if (length(ind) > 0) {
+      to_add <- matrix(ncol = ncol(x), nrow = length(ind))
+      to_add[,] <- 0
+      colnames(to_add) <- colnames(x)
+      to_add <- as.data.frame(to_add)
+      to_add$relation.liesWithinLayer <- unique(x$relation.liesWithinLayer)
+      to_add$period <- milQuant_periods$order[ind]
+      new <- bind_rows(x, to_add)
+      return(new)
+    } else {
+      return(x)
+    }
+  })
+  result <- do.call(bind_rows, sp_data)
+  return(result)
+}
+
+
+
--- R/mod_tab_pottery_QB.R
+++ R/mod_tab_pottery_QB.R
@@ -22,21 +22,30 @@ mod_pottery_QB_ui <- function(id, tabname) {
         textInput(inputId = ns("subtitle"), label = "Subtitle",
                   placeholder = "Enter subtitle here"),
         uiLayerSelector(ns("layers")),
+        switchInput(inputId = ns("split_multiple"),
+                    label = "Split counts for multiple periods",
+                    labelWidth = "170px",
+                    size = "mini",
+                    onStatus = "success",
+                    offStatus = "danger"),
         prettyRadioButtons(inputId = ns("display_context"),
-                           label = "Display Options", icon = icon("check"),
-                           inline = FALSE, animation = "jelly",
-                           choices = list("Do not display Context" = "none",
-                                          "Context as Subplot" = "wrap")),
+                           label = "Display Layer/Context as subplot (maximum 5)", icon = icon("check"),
+                           inline = TRUE, animation = "jelly",
+                           choices = list("yes" = TRUE,
+                                          "no" = FALSE),
+                           selected = FALSE),
         prettyRadioButtons(inputId = ns("display_xaxis"),
                            label = "Display Options", icon = icon("check"),
-                           inline = FALSE, animation = "jelly",
-                           choices = list("Function on x-axis" = "function",
-                                          "Period on x-axis" = "period")),
-        prettyRadioButtons(inputId = ns("display_bars"),
-                           label = "Position of Bars", icon = icon("check"),
                            inline = TRUE, animation = "jelly",
-                           choices = list("Dodged Bars" = "dodge",
-                                          "Stacked Bars" = "stack")),
+                           choices = list("Period on x-axis" = "period",
+                                          "Function on x-axis" = "function"),
+                           selected = "period"),
+        prettyRadioButtons(inputId = ns("bar_display"),
+                           label = "Choose how to display the bars",
+                           icon = icon("check"),
+                           inline = TRUE, animation = "jelly",
+                           choices = list("stacked" = "stack",
+                                          "dodging" = "group")),
         uiPeriodSelector(ns("periods")),
         downloadPlotButtons(ns("download"))
       ),
@@ -82,34 +91,68 @@ mod_pottery_QB_serv <- function(id) {
 
       generatePeriodSelector("periods", inputid = ns("selected_periods"))
 
+      debounced_selected_layers <- reactive({ input$selected_layers }) %>%
+        debounce(2000)
+
       plot_data <- reactive({
         validate(
           need(is.data.frame(potteryQB()), "Data not available.")
         )
         existing_cols <- colnames(potteryQB())
-        keep <- existing_cols
-        keep <- keep[grepl("count", keep)]
+        countcols <- existing_cols
+        countcols <- countcols[grepl("count", countcols)]
         #keep
         # remove "countTotal" as well
-        keep <- keep[!grepl("countTotal", keep)]
+        countcols <- countcols[!grepl("countTotal", countcols)]
         # needed for melt id
-        keep <- c("identifier", keep, "relation.liesWithinLayer", "period", "period.start", "period.end")
+        keep <- c("identifier", countcols, "relation.liesWithinLayer", "period", "period.start", "period.end")
 
         plot_data <- potteryQB() %>%
-          select(all_of(keep))
-
-
-        plot_data <- plot_data %>%
-          filter(relation.liesWithinLayer %in% input$selected_layers) %>%
+          select(all_of(keep)) %>%
+          filter(relation.liesWithinLayer %in% debounced_selected_layers()) %>%
           period_filter(is_milet = is_milet,
                         selector = input$selected_periods) %>%
-          melt(id = c("identifier", "relation.liesWithinLayer", "period", "period.start", "period.end")) %>%
-          mutate(value = ifelse(is.na(value), 0, value)) %>%
-          mutate(value = as.numeric(value)) %>%
-          mutate(variable = gsub("count", "", variable)) %>%
-          mutate(variable = gsub("Rim|Base|Handle|Wall", "", variable)) %>%
-          uncount(value) %>%
-          mutate(variable = fct_infreq(variable))
+          mutate(relation.liesWithinLayer = droplevels(relation.liesWithinLayer)) %>%
+          mutate_at(countcols, as.numeric) %>%
+          mutate_at(countcols, ~replace_na(., 0)) %>%
+          multiply_resources_by_period_range(split = input$split_multiple)
+
+        remove_cols <- c("period.start", "period.end", "identifier")
+
+        if (input$display_context) {
+          melt_by <- c("relation.liesWithinLayer", "period")
+        } else {
+          melt_by <- "period"
+        }
+
+        plot_data <- plot_data %>%
+          select(!all_of(remove_cols)) %>%
+          add_all_periods_to_all_contexts()
+
+        if (input$display_context == FALSE) {
+          plot_data <- plot_data %>%
+            select(!relation.liesWithinLayer)
+        }
+
+        plot_data <- plot_data %>%
+          pivot_longer(cols = !all_of(melt_by), names_to = "funGroup") %>%
+          mutate(funGroup = gsub("count", "", funGroup)) %>%
+          mutate(funGroup = gsub("Rim|Base|Handle|Wall", "", funGroup)) %>%
+          mutate(funGroup = as.factor(funGroup)) %>%
+          group_by(across(c(-value))) %>%
+          summarise(value = sum(value))
+
+        if (input$display_xaxis == "function") {
+          plot_data <- plot_data %>%
+            rename(x_axis = funGroup,
+                   color = period)
+        } else if (input$display_xaxis == "period") {
+          plot_data <- plot_data %>%
+            rename(x_axis = period,
+                   color = funGroup)
+        }
+
+
         return(plot_data)
       })
 
@@ -123,37 +166,90 @@ mod_pottery_QB_serv <- function(id) {
                               paste(input$selected_layers, collapse = ", "),
                               sep = "")
         } else {
-          plot_title <- input$title
+          plot_title <- paste0('<b>', input$title, '</b><br>', input$subtitle)
         }
+
+        data("milQuant_periods")
+
+        if (input$display_xaxis == "function") {
+          legend_title <- "Period"
+          x_title <- "Functional Group"
+          custom_colors <- unlist(milQuant_periods$colors)
+          custom_hovertemplate <- milQuant_count_hovertemplate
+
+        } else if (input$display_xaxis == "period") {
+          legend_title <- "Functional Group"
+          x_title <- "Period"
+          custom_colors <- viridis(length(unique(plot_data()$color)))
+          custom_hovertemplate <- milQuant_count_hovertemplate
+        }
+
+        if (input$display_context) {
+          max_y <- max(plot_data()$value, na.rm = TRUE)
+          max_y <- max_y * 1.25
+          sp_plot_data <- split(plot_data(), plot_data()$relation.liesWithinLayer)
+          if (length(sp_plot_data) > 5) {
+            sp_plot_data <- sp_plot_data[1:5]
+          }
+          figs <- lapply(sp_plot_data, function(pd) {
+            subfig <- plot_ly(pd,
+                    x = ~x_axis, y = ~value, color = ~color,
+                    type = "bar",
+                    colors = custom_colors,
+                    hovertemplate = custom_hovertemplate,
+                    legendgroup = ~color) %>%
+              layout(annotations = list(showarrow = F, y = max_y, x = 0,
+                                        text = paste0("<b>", unique(pd$relation.liesWithinLayer), "</b>")),
+                     yaxis = list(range = c(0, max_y),
+                                  title = "count"))
+            if (identical(pd, sp_plot_data[[1]])) {
+              subfig <- style(subfig, showlegend = TRUE)
+            } else {
+              subfig <- style(subfig, showlegend = FALSE)
+            }
+            return(subfig)
+          })
+
+          fig <- subplot(figs, nrows = length(figs), shareX = TRUE, shareY = TRUE)
+        } else {
+          fig <- plot_ly(plot_data(),
+                         x = ~x_axis, y = ~value, color = ~color,
+                         type = "bar",
+                         colors = custom_colors,
+                         hovertemplate = custom_hovertemplate)
+        }
+
 
 
         if (input$display_xaxis == "function") {
-          p <- ggplot(plot_data(), aes(x = variable,
-                                                  fill = period)) +
-            labs(x = "Vessel Forms", y = "count") +
-            scale_fill_period(ncol = 9)
+          fig <- fig %>%
+            layout(xaxis = list(title = x_title,
+                                categoryorder = "total descending"),
+                   legend = list(title = list(text = legend_title),
+                                 categoryorder = levels(milQuant_periods$order)))
         } else if (input$display_xaxis == "period") {
-          p <- ggplot(plot_data(), aes(x = period,
-                                                  fill = variable)) +
-            scale_fill_discrete(name = "Function", guide = "legend") +
-            labs(x = "Period", y = "count")
+          fig <- fig %>%
+            layout(xaxis = list(title = x_title,
+                                categoryorder = levels(milQuant_periods$order)),
+                   legend = list(title = list(text = legend_title),
+                                 categoryorder = "total descending"))
         }
 
 
-        if (input$display_context == "wrap") {
-          p <- p + facet_wrap(~ relation.liesWithinLayer, ncol = 2)
-        }
+        caption <- paste0("Total: ", sum(plot_data()$value))
 
-        p <- p +
-          labs(title = plot_title,
-               subtitle = input$subtitle,
-               caption = paste("Total Number of Fragments:", nrow(plot_data()))) +
-          geom_bar(position = input$display_bars)
-        p
+        fig <- fig %>% layout(barmode = input$bar_display,
+                              title = list(text = plot_title),
+                              yaxis = list(title = "count"))
+
+        fig <- milquant_plotly_layout(fig, caption = caption)
+
+        return(fig)
+
       })
 
       output$display_plot <- renderPlotly({
-        convert_to_Plotly(make_plot())
+        make_plot()
       })
 
       makeDownloadPlotHandler("download", dlPlot = display_plot)
