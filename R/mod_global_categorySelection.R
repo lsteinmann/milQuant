ec4910b0bbd7499f7ec08238b6a25c0d85940af9
Lisa Steinmann <37840007+lsteinmann@users.noreply.github.com>
2023-12-15 17:45:19 +0100

    make choices a grouped list in category selector

--- R/mod_global_categorySelection.R
+++ R/mod_global_categorySelection.R
@@ -30,14 +30,17 @@ generateCategorySelector <- function(id, inputid, parent = "Find", selected = "a
 
       label <- "Choose one or many resource categories"
 
-      data("milQuant_cats")
 
       output$ui_category_selector <- renderUI({
+        data("milQuant_cats")
+
+        sel_cats <- names(milQuant_cats) %in% parent
+        sel_cats <- unlist(milQuant_cats[sel_cats], use.names = FALSE)
 
         available_cats <- react_index() %>%
           filter(Operation %in% db_selected_operations()) %>%
-          filter(category %in% milQuant_cats[[parent]]) %>%
-          mutate(category = factor(category, milQuant_cats[[parent]])) %>%
+          filter(category %in% sel_cats) %>%
+          mutate(category = factor(category, sel_cats)) %>%
           pull(category) %>%
           unique() %>%
           sort()
@@ -46,14 +49,28 @@ generateCategorySelector <- function(id, inputid, parent = "Find", selected = "a
           selected <- db_selected_categories()
         } else if (selected == "all") {
           selected <- available_cats
+        } else if (selected == "none") {
+          selected <- NA
         } else {
-          match.arg(selected, choices = milQuant_cats[[parent]], several.ok = TRUE)
+          match.arg(selected, choices = sel_cats, several.ok = TRUE)
         }
 
+        available_cats_list <- lapply(milQuant_cats, function (x) {
+          x[x %in% available_cats]
+        })
+
+        pick_list <- sapply(available_cats_list, length) > 0
+        available_cats_list <- available_cats_list[pick_list]
+
+        available_cats_list <- lapply(available_cats_list, function(x) {
+          x <- as.list(x)
+          names(x) <- unlist(x)
+          x
+        })
 
         pickerInput(inputId = inputid,
                     label = label,
-                    choices = available_cats,
+                    choices = available_cats_list,
                     multiple = TRUE,
                     selected = selected,
                     options = list("actions-box" = TRUE,
